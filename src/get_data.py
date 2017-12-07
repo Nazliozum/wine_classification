f4c0859c195e96bb62417b7f6dcfdf4a4ee0ba93
Nazli Ozum Kafaee <nazliozum@gmail.com>
2017-12-07 14:45:18 -0800

    Modify get_data.py in a way that it gets arguments now

--- src/get_data.py
+++ src/get_data.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('target_folder')
+
+args = parser.parse_args()
 
 wine = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header = None)
 
-wine.to_csv("data/wine_data.csv", encoding='utf-8', index=False, header=False)
+wine.to_csv(args.target_folder, encoding='utf-8', index=False, header=False)
